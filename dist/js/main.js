ce0fd65407a7dfc399b5cd646ed7a4ab8ee389c1
tomgastall <tomgastall@gmail.com>
2020-09-04 00:38:24 -0700

    contact

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,37 +1,33 @@
+// Select DOM Items
 const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
-const navItems = document.querySelectorAll('.nav-items');
+const navItems = document.querySelectorAll('.nav-item');
 
-
-// set initial state of the menu
+// Set Initial State Of Menu
 let showMenu = false;
 
 menuBtn.addEventListener('click', toggleMenu);
 
 function toggleMenu() {
-  if(!showMenu) {
+  if (!showMenu) {
     menuBtn.classList.add('close');
     menu.classList.add('show');
     menuNav.classList.add('show');
     menuBranding.classList.add('show');
     navItems.forEach(item => item.classList.add('show'));
-  
-//Reset the menu state
-showMenu = true;
-} else {
+
+    // Set Menu State
+    showMenu = true;
+  } else {
     menuBtn.classList.remove('close');
     menu.classList.remove('show');
     menuNav.classList.remove('show');
     menuBranding.classList.remove('show');
     navItems.forEach(item => item.classList.remove('show'));
-  
-//Reset the menu state
-showMenu = false;
 
-
-
-
+    // Set Menu State
+    showMenu = false;
   }
 }
